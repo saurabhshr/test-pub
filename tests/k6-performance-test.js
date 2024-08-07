50f92205a346d1c9b273a2c67573f890865d6eb6
Saurabh <saurabh.sharma@techaheadcorp.com>
2024-08-07 19:09:50 +0530

    Create k6-performance-test.js

---
+++ tests/k6-performance-test.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check } from 'k6';
+
+export let options = {
+  vus: 10, // number of virtual users
+  duration: '30s', // duration of the test
+};
+
+export default function () {
+  const url = __ENV.TEST_URL; // get the URL from the environment variable
+  const res = http.get(`${url}`);
+  
+  check(res, {
+    'is status 200': (r) => r.status === 200,
+    'response time is less than 200ms': (r) => r.timings.duration < 200,
+  });
+}
